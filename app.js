d850c7d66ab24dcad9b99de983ba275beb8f5ffd
vijay2210 <vy9892@gmail.com>
2025-09-06 16:00:50 +0530

    add projects

--- app.js
+++
@@ -1,8 +0,0 @@
-let input = document.querySelector("input");
-let heading = document.querySelector("h2");
-
-input.addEventListener("input" , ()=> {
-    const text = input.value;
-    const onlyLetter = text.replace(/[^a-zA-Z]/g,"");
-    heading.textContent = onlyLetter;
-});
